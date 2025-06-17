8477f0a047ea36e2cda5657d42d4b55f4e6d9f2d
iamshobhraj <iamshobhraj@gmail.com>
2025-06-18 01:19:43 +0530

    init2

---
+++ scripts/notify_team_new_comment.js
@@ -0,0 +1,55 @@
+function hasLabel(labels, name) {
+  return labels.some(label => label.toLowerCase() === name.toLowerCase());
+}
+
+module.exports = async ({ github, context, core }) => {
+  try {
+    const issueNumber = context.payload.issue.number;
+    const issueUrl = context.payload.issue.html_url;
+    const issueTitle = context.payload.issue.title;
+    const escapedTitle = issueTitle.replace(/"/g, '\\"');
+    const commentId = context.payload.comment.id;
+    const commentAuthor = context.payload.comment.user.login;
+    const repo = context.repo.repo;
+    const owner = context.repo.owner;
+    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+    const communityWebhookUrl = process.env.SLACK_COMMUNITY_NOTIFICATIONS_WEBHOOK_URL;
+
+    const Maintainers = ['user1', 'user2', 'user3'];
+
+    if (Maintainers.includes(commentAuthor)) {
+      core.setOutput('isMaintainer', true);
+      return;
+    } else {
+      core.setOutput('isMaintainer', false);
+    }
+
+    const { data: labels } = await github.issues.listLabelsOnIssue({
+      owner,
+      repo,
+      issue_number: issueNumber
+    });
+
+    const labelNames = labels.map(label => label.name);
+      let message;
+    if (hasLabel(labelNames, 'help wanted')) {
+      message = `*[${repo}] New comment on issue: <${issueUrl}#issuecomment-${commentId}|${escapedTitle} by ${commentAuthor}>*`;
+      core.setOutput('webhook_url', slackWebhookUrl);
+    } else {
+      /*bot reply code here
+
+
+
+      */
+
+
+      message = `*[${repo}] New comment on issue: <${issueUrl}#issuecomment-${commentId}|${escapedTitle} by ${commentAuthor}>*`;
+      core.setOutput('webhook_url', communityWebhookUrl);
+    }
+
+    core.setOutput('text', message);
+
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error.message}`);
+  }
+};
