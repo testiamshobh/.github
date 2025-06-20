c05cbfa0c1ceb8a3d8ef18182e0efea918a3e85a
iamshobhraj <iamshobhraj@gmail.com>
2025-06-20 21:56:57 +0530

    init2

--- scripts/notify_team_new_comment.js
+++ scripts/notify_team_new_comment.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 function hasLabel(labels, name) {
   return labels.some(label => label.toLowerCase() === name.toLowerCase());
 }
@@ -10,12 +13,18 @@ module.exports = async ({ github, context, core }) => {
     const escapedTitle = issueTitle.replace(/"/g, '\\"');
     const commentId = context.payload.comment.id;
     const commentAuthor = context.payload.comment.user.login;
+    const comment = context.payload.comment.body;
     const repo = context.repo.repo;
     const owner = context.repo.owner;
     const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
     const communityWebhookUrl = process.env.SLACK_COMMUNITY_NOTIFICATIONS_WEBHOOK_URL;
 
     const Maintainers = ['user1', 'user2', 'user3'];
+    const keywordsPath = path.join(__dirname, 'keywords.txt');
+    const keywords = fs.readFileSync(keywordsPath, 'utf-8')
+        .split('\n')
+        .map(k => k.trim().toLowerCase())
+        .filter(Boolean);
 
     if (Maintainers.includes(commentAuthor)) {
       core.setOutput('isMaintainer', true);
@@ -36,11 +45,24 @@ module.exports = async ({ github, context, core }) => {
       message = `*[${repo}] New comment on issue: <${issueUrl}#issuecomment-${commentId}|${escapedTitle} by ${commentAuthor}>*`;
       core.setOutput('webhook_url', slackWebhookUrl);
     } else {
-      /*bot reply code here
 
+      const matchedKeywords = keywords.find(keyword => comment.toLowerCase().includes(keyword));
+      if(matchedKeywords){
+          github.rest.issues.createComment({
+              owner,
+              repo,
+              issue_number: issueNumber,
+              body: `Hi @${commentAuthor} 👋
 
+              Thanks so much for your interest! This issue is currently reserved for the core team and isn’t available for assignment right now.
 
-      */
+              If you’d like to get started contributing, please take a look at our [Contributing Guidelines](https://github.com/your-org/your-repo/blob/main/CONTRIBUTING.md) for tips on finding “help-wanted” issues, setting up your environment, and submitting a PR.
+
+              We really appreciate your willingness to help — feel free to pick another issue labeled **help-wanted** and let us know if you have any questions. 😊
+              `
+          });
+      }
+
 
 
       message = `*[${repo}] New comment on issue: <${issueUrl}#issuecomment-${commentId}|${escapedTitle} by ${commentAuthor}>*`;
