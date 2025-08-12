2b7d6d4ddca9296347ee5999d5d970631c4ca405
iamshobhraj <iamshobhraj@gmail.com>
2025-08-12 23:10:01 +0530

    update

--- scripts/notify_team_new_comment.js
+++ scripts/notify_team_new_comment.js
@@ -79,8 +79,8 @@ module.exports = async ({ github, context, core }) => {
         return response;
     }
 
-
-    if ( process.env.IS_CLOSE_CONTRIBUTOR || hasLabel(ISSUE_LABEL_HELP_WANTED) ) {
+    if ( process.env.IS_CLOSE_CONTRIBUTOR || await hasLabel(ISSUE_LABEL_HELP_WANTED) ) {
+      core.setInfo("Setting up notifications for contributors ", hasLabel(ISSUE_LABEL_HELP_WANTED));
       core.setOutput('webhook_url', supportDevSlackWebhookUrl);
     } else {
       core.setInfo("Setting up notifications for non-contributors");
